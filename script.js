754331490aa535dc85c861c2131f84a7ef8cef5e
MOHAMMAD-ALSUBAIE <68867495+MOHAMMAD-ALSUBAIE@users.noreply.github.com>
2022-08-15 14:26:51 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+let Typenumber = "arab";
+
+const clock = document.querySelector(".clock");
+const Arabic = document.querySelector("#Arabic");
+const English = document.querySelector("#English");
+const option = {
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+    hour12: false,
+};
+Arabic.addEventListener("click", function () {
+    Typenumber = "arab";
+    option.numberingSystem = Typenumber;
+    clockStart();
+});
+English.addEventListener("mouseover", function () {
+    Typenumber = "english";
+    option.numberingSystem = Typenumber;
+    clockStart();
+});
+
+//MobliePhone actions
+
+Arabic.addEventListener("touchstart", function (e) {
+    e.preventDefault();
+
+    Typenumber = "arab";
+    option.numberingSystem = Typenumber;
+    clockStart();
+});
+English.addEventListener("touchstart", function (e) {
+    e.preventDefault();
+    Typenumber = "english";
+    option.numberingSystem = Typenumber;
+    clockStart();
+});
+
+const clockStart = function () {
+    const now = new Date();
+    clock.textContent = new Intl.DateTimeFormat(
+        navigator.language,
+        option
+    ).format(now);
+};
+clockStart();
+setInterval(clockStart, 1000);
